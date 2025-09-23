708ab27f60eba1c50dacd29daea329b337090276
ViniciusCardoso19 <vinisilvacardoso10@gmail.com>
2025-09-23 19:42:37 -0300

    adicionado a pasta aulas

---
+++ Aulas/Aula4/Exemplo1/Exercícios/exercicios.js
@@ -0,0 +1,12 @@
+/* Exercício 1 */
+
+const nome = "Vinícius" 
+
+const sobrenome = "Cardoso"
+
+const idade = 16
+
+let estudante = true
+
+
+console.log("Olá","Meu nome é", nome, sobrenome, "e eu tenho", idade, "anos")
---
+++ Aulas/Aula4/Exemplo1/script.js
@@ -0,0 +1,17 @@
+/* Exemplo 1*/
+
+/* Vinícius Cardoso */
+
+//comentário de 1 linha
+
+console.log("Olá Mundo!")
+
+console.log("Olá Mundo novamente!")
+
+prompt("Qual é o seu nome?")
+
+
+const nome = "Vinícius"
+const idade = 16
+
+console.log("O nome é:", nome, "e sua idade é:", idade)
