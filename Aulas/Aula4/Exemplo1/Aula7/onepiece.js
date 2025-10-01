25f972839183bd2f33498112c5a0515a40792126
ViniciusCardoso19 <vinisilvacardoso10@gmail.com>
2025-09-30 21:47:36 -0300

    Adicionado a pasta Aulas

---
+++ Aulas/Aula4/Exemplo1/Aula7/onepiece.js
@@ -0,0 +1,34 @@
+ouroTotal = 75000
+membrosTripulacao = 9
+recompensaBaseLuffy = 3000000000
+aumentoRecompensa = 150000000
+bonusExperiencia = 500
+forcaTripulacao = 95
+
+ouroPorMembro = ouroTotal / membrosTripulacao
+restoOuro = ouroTotal%membrosTripulacao
+novaRecompensaLuffy = recompensaBaseLuffy + aumentoRecompensa
+ let poderLuffy = recompensaBaseLuffy*bonusExperiencia
+ reducaoAumento = aumentoRecompensa - 50000000
+
+ console.log(ouroPorMembro)
+ console.log(restoOuro)
+ console.log(novaRecompensaLuffy)
+ console.log(poderLuffy)
+ console.log(reducaoAumento)
+
+ forçaInimigo = 120
+ chanceVitoria = 0.7
+ limiteSeguranca = 0.8
+ temAkumaNoMi = true
+ inimigoTemHaki = true
+ combateJusto = "sim"
+ combateRapido = "sim"
+
+ podeVencer = forcaTripulacao>=forçaInimigo
+ riscoAlto = chanceVitoria<limiteSeguranca
+ batalhaFacil = combateJusto==combateRapido
+
+ console.log(podeVencer)
+ console.log(riscoAlto)
+ console.log(batalhaFacil)
--- Aulas/Aula4/Exemplo1/Aula7/script.js
+++ Aulas/Aula4/Exemplo1/Aula7/script.js
@@ -12,7 +12,7 @@ const segundoValor = 25
 
 const novoResultado = primeiroValor * segundoValor
 
-console.log(novoResultado)*/
+console.log(novoResultado)
 
 const primeiroValor = 345
 const segundoValor = 10
@@ -31,4 +31,51 @@ console.log(resultadoSimples)
 resultadoSimples *= 2
 console.log(resultadoSimples)
 resultadoSimples /= 3
-console.log(resultadoSimples)
+console.log(resultadoSimples)*/
+
+const primeiroResultado = 3 + 4
+const segundoResultado = 3*5 /2
+const numero1 = 5
+const numero2 = 4
+resultado1 = numero1 - numero2
+resultado2 = resultado1*-1
+const quartoResultado = 234%5
+
+console.log(primeiroResultado, segundoResultado, resultado2, quartoResultado)
+
+console.log("1"==="2")
+console.log("2"==="2")
+console.log(2==="2")
+
+console.log("1"!=="2")
+console.log("2"!=="2")
+console.log(2!=="2")
+
+console.log(1>2)
+console.log(2>2)
+console.log(3>2)
+
+console.log(1>=2)
+console.log(2>=2)
+console.log(3>=2)
+
+console.log(1<2)
+console.log(2<2)
+console.log(3<2)
+
+console.log(1<=2)
+console.log(2<=2)
+console.log(3<=2)
+
+let firstNumber = prompt("Escolha um número")
+let secondNumber = prompt("Escolha outro número")
+
+console.log("O primeiro numero é igual ao segundo?", firstNumber===secondNumber)
+console.log("O primeiro numero é diferente do segundo?", firstNumber!== secondNumber)
+console.log("O primeiro numero é maior que o segundo?", firstNumber>secondNumber)
+console.log("O primeiro numero é menor que o segundo?", firstNumber<secondNumber)
+
+
+
+
+
