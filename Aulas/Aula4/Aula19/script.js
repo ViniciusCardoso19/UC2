2f9ce8429825d1388c2792834c8b9a3aa978469f
ViniciusCardoso19 <vinisilvacardoso10@gmail.com>
2025-11-17 21:50:12 -0300

    Adicionado a pasta Aulas

---
+++ Aulas/Aula4/Aula19/script.js
@@ -0,0 +1,40 @@
+/*let matriz = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+ matriz[1][2] = 1
+
+ console.log(matriz)
+*/
+
+/*let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+let soma = 0
+for(let i = 0; i<matriz.length; i++){
+    for(let j = 0; j<matriz[i].length;j++){
+      soma += matriz[i][j]
+    }
+}
+console.log(soma)*/
+
+let matriz = [
+    [1, 2, 3, 4], 
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+function percorrerSoma(){
+    let soma = 0
+    for(let i = 0; i<matriz.length;i++){
+        for(let j = 0; j<matriz.length;j++){
+            soma+= matriz[i][j]
+            console.log(soma)
+        }
+    }
+}
+percorrerSoma()
